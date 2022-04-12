080deae3d918a97e632ca638c4d8a1d4c5630193
Luis Berenguer <luis01developer@gmail.com>
2022-04-12 17:25:01 +0200

    no me coge la ruta api/auth/luis

--- pages/api/auth/luis.js
+++ pages/api/auth/luis.js
@@ -8,7 +8,7 @@ const credentials = async (req, res) => {
     }
     return res.status(200).json(luisUser)
   }
-  res.status(401).end('No era el tres amigo mio')
+  res.status(401).end('No era el tres amigo mio!')
 }
 
 export default credentials
